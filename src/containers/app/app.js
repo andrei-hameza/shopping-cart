8c9115c415602041f023439a6e907f1c1677f990
andrei-hameza <hameza@gmail.com>
2017-05-24 01:42:35 +0300

    Moves App component to separate folder

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './app.css';
 
 class App extends Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './containers/app';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './index.css';
 
