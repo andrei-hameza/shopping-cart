edb4f7802d1467feaf6a6686dd0812be25ba63d6
andrei-hameza <hameza@gmail.com>
2017-05-24 02:23:33 +0300

    Integrates redux store to application

---
+++ src/containers/app/reducer.js
@@ -0,0 +1,5 @@
+const testReducer = (state = {}, action) => {
+  return state;
+}
+
+export default testReducer;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createLogger } from 'redux-logger';
+import appReducer from './containers/app/reducer';
 import App from './containers/app';
 import registerServiceWorker from './utils/registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const middleware = [];
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger());
+}
+
+const store = createStore(
+  appReducer,
+  applyMiddleware(...middleware)
+)
+
+// store.dispatch(getAllProducts())
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
+
 registerServiceWorker();
