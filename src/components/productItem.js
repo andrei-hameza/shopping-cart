903d184a6e9a54568ef7ee9ac1a964ac8d04cfbf
andrei-hameza <hameza@gmail.com>
2017-05-27 13:58:26 +0300

    Adds ProductItem component

---
+++ src/components/productItem.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProductItem = ({ product, onAddToCart }) => {
+  const name = product.get('name')
+  const price = product.get('price')
+
+  return (
+    <div className='product'>
+      <h4 className='product__name'>
+        {name}
+      </h4>
+      <span className='product__price'>
+        {price}
+      </span>
+      <button onClick={onAddToCart}>
+        {'Add'}
+      </button>
+    </div>
+  )
+}
+
+ProductItem.propTypes = {
+  product: PropTypes.shape({
+    title: PropTypes.string,
+    price: PropTypes.number
+  }),
+  onAddToCartClicked: PropTypes.func
+}
+
+export default ProductItem
