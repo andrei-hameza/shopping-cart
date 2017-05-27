8c716888efdf2c0b0f0179afefe15552985541c8
andrei-hameza <hameza@gmail.com>
2017-05-27 13:55:43 +0300

    Removes legacy code

--- src/containers/app/app.js
+++ src/containers/app/app.js
@@ -5,10 +5,7 @@ import './app.css'
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <div className='App-header'>
-          <h2>Welcome to React</h2>
-        </div>
+      <div className='shop'>
         <ProductsContainer />
       </div>
     )
