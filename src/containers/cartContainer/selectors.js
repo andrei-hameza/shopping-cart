0ae201bde913c761ef1eb34c57e0d65dda9cf323
andrei-hameza <hameza@gmail.com>
2017-05-27 21:23:44 +0300

    Refactoring cartContainer selectors to preserve valid products order in cart

--- src/containers/cartContainer/selectors.js
+++ src/containers/cartContainer/selectors.js
@@ -10,9 +10,17 @@ const cartProducts = createSelector([products, cartProductsIds], (products, ids)
   if (ids.size === 0) {
     return Immutable.List()
   }
-  return products
-    .filter(product => ids.get(product.get('id')))
-    .map(product => product.set('count', ids.get(product.get('id'))))
+
+  const result = ids.reduce((acc, count, id) => {
+    const product = products.find(product => product.get('id') === id)
+    const productId = product.get('id')
+    const cartProduct = product.set(
+      'count',
+      ids.get(productId)
+    )
+    return acc.push(cartProduct)
+  }, Immutable.List())
+  return result
 })
 
 export function cartContainerSelector (state) {
