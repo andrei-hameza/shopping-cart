75f72a5315c892e15fa73b6d3d454715497d6e33
andrei-hameza <hameza@gmail.com>
2017-05-27 13:59:26 +0300

    Refactoring ProductsContainer component: ProductsList and ProductItem were added to it

--- src/containers/productsContainer/productsContainer.js
+++ src/containers/productsContainer/productsContainer.js
@@ -1,14 +1,44 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import Immutable from 'immutable'
+
+import ProductsList from '../../components/productsList'
+import ProductItem from '../../components/productItem'
+
 import {
   productsContainerSelector
 } from './selectors'
 
-const ProductsContainer = ({ products }) => {
-  const items = products.map((product) => (<div key={product.get('id')}>{product.get('name')}</div>))
-  return (<div>{items}</div>)
+const ProductsContainer = ({ products = Immutable.List, addToCart }) => {
+  const productItems = products.map((product) => (
+    <li
+      className='products-list__item'
+      key={product.get('id')}>
+      <ProductItem
+        product={product}
+        onAddToCart={() => addToCart('1')} />
+    </li>
+  ))
+
+  return (
+    <ProductsList>
+      <ul className='products-list'>
+        {productItems}
+      </ul>
+    </ProductsList>
+  )
 }
 
+const addToCart = id => (
+  {
+    type: 'MMM',
+    payload: null
+  }
+)
+
 export default connect(
-  productsContainerSelector
+  productsContainerSelector,
+  {
+    addToCart
+  }
 )(ProductsContainer)
