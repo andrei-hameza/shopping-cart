59f5dc87e9b9096f4b13ff3a3cf0835961111c06
andrei-hameza <hameza@gmail.com>
2017-05-30 01:31:39 +0300

    Adds comments for productsContainer selectors

--- src/containers/productsContainer/selectors.js
+++ src/containers/productsContainer/selectors.js
@@ -1,9 +1,34 @@
+import { createStructuredSelector } from 'reselect'
+
+/**
+ * Give the oredered map of all products | { id: product }
+ *
+ * @param {Immutable.Map} [state]
+ * @return {Immutable.OrderedMap}
+ */
+
 export function products (state) {
   return state.getIn(['products', 'data'])
 }
 
-export function productsContainerSelector (state) {
-  return {
-    products: products(state).toList()
-  }
+/**
+ * Give the list of all products
+ *
+ * @param {Immutable.Map} [state]
+ * @return {Immutable.List}
+ */
+
+export function productsList (state) {
+  return state.getIn(['products', 'data']).toList()
 }
+
+/**
+* Structured selector that returns data for productsContainer compopnent | mapStateToProps
+*
+* @param {Immutable.Map} [state]
+* @return {Object}
+*/
+
+export const productsContainerSelector = createStructuredSelector({
+  products: productsList
+})
