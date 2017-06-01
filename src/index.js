a2c58a32b9d441cd48e785417a04a416098707b1
andrei-hameza <hameza@gmail.com>
2017-06-02 01:26:11 +0300

    Moves store configuration to separate file

--- src/index.js
+++ src/index.js
@@ -1,54 +1,17 @@
+// libs
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Immutable from 'immutable'
-import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
-import { createLogger } from 'redux-logger'
-import thunk from 'redux-thunk'
-import appReducer from './containers/app/reducer'
+
+// components
 import App from './containers/app'
+
+// helpers
 import registerServiceWorker from './utils/registerServiceWorker'
+import store from './store/configureStore'
+
+// styles
 import './styles/index.css'
-
-const middleware = [ thunk ]
-
-if (process.env.NODE_ENV !== 'production') {
-  middleware.push(createLogger())
-}
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-const enhancer = composeEnhancers(
-  applyMiddleware(...middleware)
-)
-
-let persistedState
-
-// TODO: move to separate enhancer and check private mode
-try {
-  persistedState = window.localStorage.getItem('shoppingCartApp')
-    ? JSON.parse(window.localStorage.getItem('shoppingCartApp')) || {}
-    : {}
-} catch (e) {
-  persistedState = {}
-}
-
-const store = createStore(
-  appReducer,
-  Immutable.fromJS(persistedState),
-  enhancer
-)
-
-store.subscribe(() => {
-  window.localStorage
-    .setItem('shoppingCartApp', JSON.stringify({
-      cart: {
-        data: store.getState().getIn(['cart', 'data']).toJS(),
-        sorting: {},
-        status: ''
-      }
-    })
-  )
-})
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/configureStore.js
@@ -0,0 +1,47 @@
+import { createLogger } from 'redux-logger'
+import thunk from 'redux-thunk'
+import Immutable from 'immutable'
+import { createStore, applyMiddleware, compose } from 'redux'
+import appReducer from '../containers/app/reducer'
+
+const middleware = [ thunk ]
+
+if (process.env.NODE_ENV !== 'production') {
+  middleware.push(createLogger())
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware)
+)
+
+let persistedState
+
+// TODO: move to separate enhancer and check private mode
+try {
+  persistedState = window.localStorage.getItem('shoppingCartApp')
+    ? JSON.parse(window.localStorage.getItem('shoppingCartApp')) || {}
+    : {}
+} catch (e) {
+  persistedState = {}
+}
+
+const store = createStore(
+  appReducer,
+  Immutable.fromJS(persistedState),
+  enhancer
+)
+
+store.subscribe(() => {
+  window.localStorage
+    .setItem('shoppingCartApp', JSON.stringify({
+      cart: {
+        data: store.getState().getIn(['cart', 'data']).toJS(),
+        sorting: {},
+        status: ''
+      }
+    })
+  )
+})
+
+export default store
