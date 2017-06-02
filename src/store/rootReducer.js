2513493ec1b10f88321f083634db8890fb13e754
andrei-hameza <hameza@gmail.com>
2017-06-03 00:24:54 +0300

    Adds comments to root reducer

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -6,11 +6,13 @@ import { combineReducers } from 'redux-immutable'
 import productsReducer from '../containers/productsContainer/reducer'
 import cartReducer from '../containers/cartContainer/reducer'
 
+// application initial state
 const initialState = Immutable.Map({
   products: {},
   cart: {}
 })
 
+// application root reducer
 const rootReducer = combineReducers({
   products: productsReducer,
   cart: cartReducer
