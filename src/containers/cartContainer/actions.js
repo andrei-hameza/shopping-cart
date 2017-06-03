6a033fb226b537e30167e82109510e4f0bbb2749
andrei-hameza <hameza@gmail.com>
2017-06-04 00:57:58 +0300

    Adds comments for cartContainer actions

--- src/containers/cartContainer/actions.js
+++ src/containers/cartContainer/actions.js
@@ -1,21 +1,19 @@
-import { cartActionTypes } from '../../constants/actionTypes'
+// services
 import shoppingCartService from '../../services/shoppingCartService'
+
+// constants
+import { cartActionTypes } from '../../constants/actionTypes'
+import { randomSampleSize } from '../../constants/config'
+
+// helpers
 import generateRandomSampleFromCollection from '../../utils/generateRandomSampleFromCollection'
 
-// TODO: constants for action types
-export const removeFromCart = productId => (
-  {
-    type: cartActionTypes.REMOVE_FROM_CART,
-    payload: productId
-  }
-)
-
-export const clearCart = () => (
-  {
-    type: cartActionTypes.CLEAR_CART,
-    payload: null
-  }
-)
+/**
+ * Adds product to the cart
+ *
+ * @param  {String} productId - Product id
+ * @return {Object} - Action
+ */
 
 export const addToCart = productId => (
   {
@@ -24,16 +22,70 @@ export const addToCart = productId => (
   }
 )
 
-// TODO: constant for products amoint in autofill
+/**
+ * Removes product from the cart
+ *
+ * @param  {String} productId - Product id
+ * @return {Object} - Action
+ */
+
+export const removeFromCart = productId => (
+  {
+    type: cartActionTypes.REMOVE_FROM_CART,
+    payload: productId
+  }
+)
+
+/**
+ * Clears the cart
+ *
+ * @return {Object} - Action
+ */
+
+export const clearCart = () => (
+  {
+    type: cartActionTypes.CLEAR_CART,
+    payload: null
+  }
+)
+
+/**
+ * Changes the sorting in the cart
+ * @param  {String} sortId - Sorting id: 'ascending' or 'descending'
+ * @return {Object} - Action
+ */
+
+export const changeSort = sortId => (
+  {
+    type: cartActionTypes.CHANGE_SORTING,
+    payload: sortId
+  }
+)
+
+/**
+ * Automatically fills the cart with random products
+ *
+ * @return {Function}
+ */
+
 export const autofillCart = () => (dispatch, getState) => {
   const state = getState()
   const products = state.getIn(['products', 'data'])
-  const randomProducts = generateRandomSampleFromCollection(products.toList().toJS(), 40).map(product => product.id)
+  const randomProducts = generateRandomSampleFromCollection(
+    products.toList().toJS(), randomSampleSize
+  ).map(product => product.id)
+
   dispatch({
     type: cartActionTypes.BATCH_ADD_TO_CART,
     payload: randomProducts
   })
 }
+
+/**
+ * Performs products purchase via appropriate service
+ *
+ * @return {Function}
+ */
 
 export const purchaseProducts = () => (dispatch, getState) => {
   const state = getState()
@@ -47,10 +99,3 @@ export const purchaseProducts = () => (dispatch, getState) => {
     setTimeout(dispatch, 1000, { type: cartActionTypes.CLEAR_PURCHASE_STATUS })
   })
 }
-
-export const changeSort = sortId => (
-  {
-    type: cartActionTypes.CHANGE_SORTING,
-    payload: sortId
-  }
-)
