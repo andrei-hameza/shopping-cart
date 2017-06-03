3891a002f4f9c52c405ad1881e89adba856e172b
andrei-hameza <hameza@gmail.com>
2017-06-04 01:00:53 +0300

    Adds comments for productsContainer

--- src/containers/productsContainer/actions.js
+++ src/containers/productsContainer/actions.js
@@ -1,5 +1,11 @@
 import { productsActionTypes } from '../../constants/actionTypes'
 
+/**
+ * Sets products data
+ * @param  {Object[]} data - Products data to dispatch
+ * @return {Function}
+ */
+
 export const receiveData = data => (dispatch) => {
   dispatch({
     type: productsActionTypes.SET_DATA,
