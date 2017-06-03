7f19a7c7ba84e2ff661f6eb65c3eb7479642390c
andrei-hameza <hameza@gmail.com>
2017-06-03 22:30:42 +0300

    Adds comments for shoppingCartService

--- src/services/shoppingCartService.js
+++ src/services/shoppingCartService.js
@@ -1,9 +1,25 @@
 import data from '../data/products.json'
 
 const shoppingCartService = {
+
+  /**
+   * Fetches products data from the server
+   *
+   * @return {Promise.<Object[]>} - Returns Promise. When resolved, it provides products data
+   */
+
   getData: () => {
     return Promise.resolve(data)
   },
+
+  /**
+   * Sends products ids from cart to the server
+   * @param  {String[]} - Products ids
+   * @return {Promise.<Object[]>} - Returns Promise for data sending.
+   * When resolved, it indicates that products data was delivered successfully
+   * When rejected, it indicates that products data wasn't delivered
+   */
+
   sendData: (products) => {
     return new Promise((resolve, reject) => {
       setTimeout(resolve, 1000)
