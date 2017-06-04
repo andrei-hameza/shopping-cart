47e3a8f3fdcead9f3757c223b911d9fea37ffe22
andrei-hameza <hameza@gmail.com>
2017-06-04 12:23:58 +0300

    Refactoring Product component: moved to pure component, added handleClick to refuse handler recreation on every render

--- src/components/product.js
+++ src/components/product.js
@@ -1,42 +1,53 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import PropTypes from 'prop-types'
 
-const Product = ({ product, addToCart }) => {
-  const name = product.get('name')
-  const snippet = product.get('snippet')
-  const price = product.get('price')
+class Product extends PureComponent {
+  handleClick = () => {
+    const {
+      id,
+      onAddToCart
+    } = this.props
+    onAddToCart(id)
+  }
 
-  return (
-    <div className='product'>
-      <div className='product__left'>
-        <h4 className='product__name'>
-          {name}
-        </h4>
-        <p className='product__info'>
-          {snippet}
-        </p>
+  render () {
+    const {
+      name,
+      snippet,
+      price
+    } = this.props
+
+    return (
+      <div className='product'>
+        <div className='product__left'>
+          <h4 className='product__name'>
+            {name}
+          </h4>
+          <p className='product__info'>
+            {snippet}
+          </p>
+        </div>
+        <div className='product__right'>
+          <span className='product__price'>
+            {`$ ${price}`}
+          </span>
+          <button
+            className='product__purchase-button'
+            onClick={this.handleClick}>
+            Add To Cart
+          </button>
+        </div>
       </div>
-      <div className='product__right'>
-        <span className='product__price'>
-          {`$ ${price}`}
-        </span>
-        <button
-          className='product__purchase-button'
-          onClick={addToCart}>
-          Add To Cart
-        </button>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 Product.propTypes = {
-  product: PropTypes.shape({
-    name: PropTypes.string,
-    snippet: PropTypes.string,
-    price: PropTypes.number
-  }),
-  addToCart: PropTypes.func.isRequired
+  id: PropTypes.string,
+  name: PropTypes.string,
+  snippet: PropTypes.string,
+  price: PropTypes.number,
+  onAddToCart: PropTypes.func
 }
 
 export default Product
